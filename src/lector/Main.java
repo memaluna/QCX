febda336854ce65780afe2a2213ef14acb44c761
Emanuel Luna <memonluna@gmail.com>
2024-12-14 21:19:55 -0300

    Implementación LOG4J

--- src/lector/Main.java
+++ src/lector/Main.java
@@ -6,27 +6,25 @@ import java.time.Month;
 
 import javax.swing.JOptionPane;
 
+import org.apache.log4j.Logger;
+
 public class Main {
+	
+	private static final Logger Log = Logger.getLogger(Main.class);
 
 	public static void main(String[] args) throws IOException {
+		
+		
 
 		IconoMensajes Ico = new IconoMensajes();
 		Ico.generarIcono();
-
-		LocalDate fechahoy = LocalDate.now();
-		LocalDate fechaCad = LocalDate.of(2021, Month.DECEMBER, 11);
-
-		if (fechahoy.equals(fechaCad)) {
-			System.out.println("fechas iguales");
-			JOptionPane.showMessageDialog(null, "Error");
-			System.exit(0);
-		} else {
-			Lector fileChangeWatcher = new Lector();
-			try {
-				fileChangeWatcher.doWath("C:\\QCX");
-			} catch (IOException e) {
-				e.printStackTrace();
-			}
+		
+		Lector fileChangeWatcher = new Lector();
+		try {
+			fileChangeWatcher.doWath("C:\\QCX");
+		} catch (IOException e) {
+			Log.info(e);
+			e.printStackTrace();
 		}
 	}
 
